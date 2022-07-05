ceafcacc8a6565f316a51e4e2e57e7c1128ac6fe
Lucas Saliés Brum <sistematico@gmail.com>
2022-07-05 16:21:35 -0400

    Alterações no branch main

---
+++ .dwm/bar.sh
@@ -0,0 +1,127 @@
+#!/usr/bin/env bash
+
+# One Dark
+bg="^c#1f2227^"
+fg="^c#abb2bf^"
+cyan="^c#88c0d0^"
+green="^c#a3be8c^"
+red="^c#bf616a^"
+yellow="^c#d7ba7d^"
+blue="^c#5e81ac^"
+magenta="^c#b48ead^"
+
+fixed() {
+    printf '%-2s' $1
+}
+
+SEP(){
+    echo -n " " # "^c#666666^$sep"
+}
+
+NEWS() {
+    [ "$(stat -c %y /tmp/news 2>/dev/null | awk '{ print substr($0, 0, 13) }')" = "$(date '+%Y-%m-%d %H')" ] || \
+    curl -sGd 'limit=1&t=all' \
+    -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" \
+    'https://www.reddit.com/r/worldnews.json' | \
+    jq '.data.children[0].data.title' | \
+    sed -e 's/^"//' -e 's/"$//' > /tmp/news
+
+    news=$(cat /tmp/news | awk -v len=50 '{ if (length($0) > len) print substr($0, 1, len-3) "..."; else print; }')
+    echo -n "${red} ${fg}${news}"
+}
+
+DOWNLOADS() {
+    inst=$(ps -ef | grep yt-dlp | grep -v grep | wc -l)
+    [ $inst -gt 0 ] && echo -n "${red} ${inst}${fg}" || echo ""
+}
+
+CRYPTO() {
+    [ "$(stat -c %y /tmp/crypto 2>/dev/null | awk '{ print substr($0, 0, 13) }')" = "$(date '+%Y-%m-%d %H')" ] || curl -sf "brl.rate.sx/1btc" > /tmp/crypto
+    crypto=$(printf "R$ %.2f" $(cat /tmp/crypto))
+    echo -n "${green} ${fg}${crypto}"
+}
+
+WEATHER() {
+    [ "$(stat -c %y /tmp/weather 2>/dev/null | awk '{ print substr($0, 0, 13) }')" = "$(date '+%Y-%m-%d %H')" ] || curl -sf 'wttr.in/Campo%20Grande?format=%t' > /tmp/weather
+    weather="$(cat /tmp/weather)"
+    echo -n "$yellow ${fg}${weather}"
+}
+
+MOON() {
+    [ "$(stat -c %y /tmp/moonfile 2>/dev/null | cut -d' ' -f1)" = "$(date '+%Y-%m-%d')" ] ||
+    { curl -sf "wttr.in/?format=%m" > /tmp/moonfile ;}
+
+    icon="$(cat /tmp/moonfile)"
+
+    case "$icon" in
+        🌑) name="nova" ;;
+        🌘) name="minguante" ;;
+        🌗) name="quarto minguante" ;;
+        🌖) name="minguante convexa" ;;
+        🌕) name="cheia" ;;
+        🌔) name="crescente convexa" ;;
+        🌓) name="quarto crescente" ;;
+        🌒) name="crescente" ;;
+        *) name="" ;;
+    esac
+
+    echo -n "${blue} ${fg}$name"
+}
+
+UPDATES() {
+    updates=$(checkupdates 2>/dev/null | wc -l)
+    
+    if [ $updates == 0 ]; then
+        echo -n "${green} ${fg}atualizado"
+    else
+        echo -n "${cyan} ${fg}${updates}"
+    fi
+}
+
+TEMP(){
+    temp=$(sensors | grep 'AMD TSI Addr 98h:' | awk 'NR==1 {print $5}' | sed 's/+//g')
+    #temp=$(sensors | grep 'Package id 0:' | awk 'NR==1 {print $4}' | sed 's/+//g')
+    echo -n "${red} ${temp}${fg}"
+}
+
+UPTIME(){
+    uptime=$(uptime --pretty | sed -e 's/up //g' -e 's/ days/d/g' -e 's/ day/d/g' -e 's/ hours/h/g' -e 's/ hour/h/g' -e 's/ minutes/m/g' -e 's/ minute/m/g' -e 's/, / /g')
+    echo -n "${green}${uptime}${fg}"
+}
+
+VOL(){
+    #vol=$(amixer sget Master | grep 'Right:' | awk -F'[][]' '{print $2}')
+    #vol=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+
+    sink=$( pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1 )
+    vol=$( pactl list sinks | grep '^[[:space:]]Volume base:' | head -n $(( $sink + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' )
+
+    echo -n "${yellow} ${vol}%${fg} "
+}
+
+CPU(){
+    cpu=$(top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+    formatedcpu=$(fixed $cpu)
+    echo -n "${blue}${formatedcpu}%${fg}"
+}
+
+MEM(){
+    mem=$(free -h | awk '/^Mem.:/ {print $3}' | sed 's/i//g')
+    echo -n "${magenta}${mem}${fg}"
+}
+
+DISK(){
+    echo -n "${red}$(df -h | awk '/home/{print $5}')${fg}"
+}
+
+TIME(){
+    time=$(date '+%I:%M%p')
+    echo -n "${cyan} ${time}${fg}"
+}
+
+while true; do
+    #xsetroot -name "$(printf '%s  %s  %s  %s  %s  %s  %s  %s  %s  %s  %s  %s  %s' "$(NEWS)" "$updates" "$downloads" "$(CRYPTO)" "$(WEATHER)" "$(MOON)" "$(TEMP)" "$(CPU)" "$(MEM)" "$(DISK)" "$(UPTIME)" "$(VOL)" "$(TIME)") "
+    xsetroot -name "$(printf '%s %s %s' "$(DOWNLOADS)" "$(TEMP)" "$(VOL)" "$(TIME)")"
+    sleep 10
+done
+
---
+++ README.md
@@ -0,0 +1,47 @@
+# dwm - dynamic window manager
+
+<p align="center">
+    <img src="dwm.png" alt="dwm" />
+</p>
+
+dwm é um gerenciador de janelas extremamente rápido, pequeno e dinâmico para o X.
+
+## Repositórios
+
+| Repositório  |  Descrição |
+| :---         |       ---: |
+| (este repositório)   | Arquivos auxiliares e patches para o dwm |
+| [unstable](https://github.com/sistematico/dwm/tree/unstable)    | Versão instável do dwm com atualizações constantes |
+
+## Usando
+
+### Clone este repositório: 
+
+```bash
+git clone -b 02-statuspadding https://sistematico@bitbucket.org/sistematico/dwm.git 
+```
+
+### Construa os arquivos de configuração:
+
+```bash
+make
+```
+
+### Instale o Gerenciador:
+
+```bash
+sudo make install clean
+```
+
+## Créditos
+
+- [dwm](https://dwm.suckless.org)
+- [Arch Linux](https://www.archlinux.org)
+
+## Ajude
+
+Se esse repositório foi útil pra você de alguma maneira(eu espero que sim :grinning:) considere doar um valor simbólico ou me patrocinar para que eu possa ajudar sempre mais.
+
+[![LiberaPay](https://img.shields.io/badge/LiberaPay-gray?logo=liberapay&logoColor=white&style=flat-square)](https://liberapay.com/sistematico/donate) [![PagSeguro](https://img.shields.io/badge/PagSeguro-gray?logo=pagseguro&logoColor=white&style=flat-square)](https://pag.ae/bfxkQW) [![ko-fi](https://img.shields.io/badge/ko--fi-gray?logo=ko-fi&logoColor=white&style=flat-square)](https://ko-fi.com/K3K32RES9) [![Buy Me a Coffee](https://img.shields.io/badge/Buy_Me_a_Coffee-gray?logo=buy-me-a-coffee&logoColor=white&style=flat-square)](https://www.buymeacoffee.com/sistematico) [![Open Collective](https://img.shields.io/badge/Open_Collective-gray?logo=opencollective&logoColor=white&style=flat-square)](https://opencollective.com/sistematico) [![Patreon](https://img.shields.io/badge/Patreon-gray?logo=patreon&logoColor=white&style=flat-square)](https://patreon.com/sistematico)
+
+![GitHub Sponsors](https://img.shields.io/github/sponsors/sistematico?label=Github%20Sponsors)
